a527dcf062bdec64eefd9ee5574a56a538e4e809
Pedro Bustamante <PedroOzoBustamante@Pedros-MacBook-Pro.local>
2017-02-15 11:54:31 -0500

    adding files

---
+++ src/main/java/AddressCount.java
@@ -0,0 +1,33 @@
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.conf.*;
+public class AddressCount {
+
+  public static void main(String[] args) throws Exception {
+    if (args.length != 3) {
+      System.err.println("Usage: Access_log <input path> <output path> <Address>");
+      System.exit(-1);
+    }
+    Configuration conf = new Configuration();
+    conf.set("address", args[2]);
+
+    Job job = new Job(conf);
+    job.setJarByClass(AccessLog.class);
+    job.setJobName("AddressCount");
+
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    
+    job.setMapperClass(AddressCountMapper.class);
+    job.setReducerClass(AddressCountReducer.class);
+
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
---
+++ src/main/java/IPCountReducer.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.conf.*;
+
+public class IPCountReducer
+    extends Reducer<Text, IntWritable, Text, IntWritable> {
+  
+  @Override
+  public void reduce(Text key, Iterable<IntWritable> values, Context context)
+      throws IOException, InterruptedException {
+    Configuration conf = context.getConfiguration();
+    if(key.equals(new Text(conf.get("IP")))) {
+
+       int sum = 0;
+    for (IntWritable value : values) {
+      sum += value.get();
+    }
+
+        context.write(key, new IntWritable(sum));        
+    }
+   
+    
+    
+  }
+}
